0baca0710c54c10fabb2d96ba63ef412f8a28ae6
Queiroz <kalifor_dbel@hotmail.com>
2023-02-05 11:00:46 -0300

    Adicionando módulo estrutura de departamentos

---
+++ GerenciadorEmpresarial/Departamentos/ListaDepartamentos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GerenciadorEmpresarial.Departamentos
+{
+    public partial class ListaDepartamentos : Form
+    {
+        public ListaDepartamentos() => InitializeComponent();
+
+        private void btnVoltar_Click(object sender, EventArgs e) => this.Close();
+    }
+}
