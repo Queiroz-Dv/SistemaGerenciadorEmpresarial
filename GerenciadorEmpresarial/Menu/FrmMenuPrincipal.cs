438f0a5e0c8fc1e45c7d12826215e3c3b0825e6c
Queiroz <kalifor_dbel@hotmail.com>
2023-02-05 10:39:29 -0300

    Incluindo estrutura do Menu Principal e alterações no Login

---
+++ GerenciadorEmpresarial/Menu/FrmMenuPrincipal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GerenciadorEmpresarial.Menu
+{
+    public partial class FrmMenuPrincipal : Form
+    {
+        public FrmMenuPrincipal()
+            => InitializeComponent();
+
+        private void btnFechar_Click(object sender, EventArgs e)
+            => Application.Exit();
+    }
+}
