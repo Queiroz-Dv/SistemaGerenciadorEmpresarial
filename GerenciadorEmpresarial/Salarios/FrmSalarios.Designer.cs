0697fde9ab625983c891f6c8d95a17f0259ad59b
Queiroz <kalifor_dbel@hotmail.com>
2023-02-10 09:05:44 -0300

    Adicionando parte da estrutura do módulo de Salários

---
+++ GerenciadorEmpresarial/Salarios/FrmSalarios.Designer.cs
@@ -0,0 +1,338 @@
+﻿
+namespace GerenciadorEmpresarial.Salarios
+{
+    partial class FrmSalarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.cmbMes = new System.Windows.Forms.ComboBox();
+            this.lblMes = new System.Windows.Forms.Label();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.lblAno = new System.Windows.Forms.Label();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtNumeroUsuario = new System.Windows.Forms.TextBox();
+            this.lblSobrenome = new System.Windows.Forms.Label();
+            this.lblNumeroUsuario = new System.Windows.Forms.Label();
+            this.cmbCargo = new System.Windows.Forms.ComboBox();
+            this.cmbDepartamento = new System.Windows.Forms.ComboBox();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.lblDepartamento = new System.Windows.Forms.Label();
+            this.dgvSalarios = new System.Windows.Forms.DataGridView();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.pnlSalarios = new System.Windows.Forms.Panel();
+            this.pnlDepartamentoCargo = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalarios)).BeginInit();
+            this.pnlSalarios.SuspendLayout();
+            this.pnlDepartamentoCargo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(198, 443);
+            this.btnVoltar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(87, 30);
+            this.btnVoltar.TabIndex = 30;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGravar.Location = new System.Drawing.Point(66, 443);
+            this.btnGravar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(95, 30);
+            this.btnGravar.TabIndex = 29;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // cmbMes
+            // 
+            this.cmbMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbMes.FormattingEnabled = true;
+            this.cmbMes.Location = new System.Drawing.Point(157, 225);
+            this.cmbMes.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbMes.Name = "cmbMes";
+            this.cmbMes.Size = new System.Drawing.Size(176, 28);
+            this.cmbMes.TabIndex = 28;
+            // 
+            // lblMes
+            // 
+            this.lblMes.AutoSize = true;
+            this.lblMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMes.Location = new System.Drawing.Point(4, 223);
+            this.lblMes.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(42, 20);
+            this.lblMes.TabIndex = 39;
+            this.lblMes.Text = "Mês";
+            // 
+            // txtAno
+            // 
+            this.txtAno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAno.Location = new System.Drawing.Point(157, 187);
+            this.txtAno.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(176, 26);
+            this.txtAno.TabIndex = 27;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalario.Location = new System.Drawing.Point(157, 149);
+            this.txtSalario.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(176, 26);
+            this.txtSalario.TabIndex = 25;
+            // 
+            // lblAno
+            // 
+            this.lblAno.AutoSize = true;
+            this.lblAno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAno.Location = new System.Drawing.Point(4, 187);
+            this.lblAno.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAno.Name = "lblAno";
+            this.lblAno.Size = new System.Drawing.Size(41, 20);
+            this.lblAno.TabIndex = 37;
+            this.lblAno.Text = "Ano";
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalario.Location = new System.Drawing.Point(4, 149);
+            this.lblSalario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(65, 20);
+            this.lblSalario.TabIndex = 38;
+            this.lblSalario.Text = "Salário";
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSobrenome.Location = new System.Drawing.Point(157, 114);
+            this.txtSobrenome.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.ReadOnly = true;
+            this.txtSobrenome.Size = new System.Drawing.Size(176, 26);
+            this.txtSobrenome.TabIndex = 33;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(157, 75);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.ReadOnly = true;
+            this.txtNome.Size = new System.Drawing.Size(176, 26);
+            this.txtNome.TabIndex = 32;
+            // 
+            // txtNumeroUsuario
+            // 
+            this.txtNumeroUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeroUsuario.Location = new System.Drawing.Point(157, 40);
+            this.txtNumeroUsuario.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNumeroUsuario.Name = "txtNumeroUsuario";
+            this.txtNumeroUsuario.ReadOnly = true;
+            this.txtNumeroUsuario.Size = new System.Drawing.Size(176, 26);
+            this.txtNumeroUsuario.TabIndex = 31;
+            // 
+            // lblSobrenome
+            // 
+            this.lblSobrenome.AutoSize = true;
+            this.lblSobrenome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSobrenome.Location = new System.Drawing.Point(4, 112);
+            this.lblSobrenome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSobrenome.Name = "lblSobrenome";
+            this.lblSobrenome.Size = new System.Drawing.Size(101, 20);
+            this.lblSobrenome.TabIndex = 34;
+            this.lblSobrenome.Text = "Sobrenome";
+            // 
+            // lblNumeroUsuario
+            // 
+            this.lblNumeroUsuario.AutoSize = true;
+            this.lblNumeroUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroUsuario.Location = new System.Drawing.Point(4, 40);
+            this.lblNumeroUsuario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNumeroUsuario.Name = "lblNumeroUsuario";
+            this.lblNumeroUsuario.Size = new System.Drawing.Size(120, 20);
+            this.lblNumeroUsuario.TabIndex = 36;
+            this.lblNumeroUsuario.Text = "Nº de Usuário";
+            // 
+            // cmbCargo
+            // 
+            this.cmbCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCargo.FormattingEnabled = true;
+            this.cmbCargo.Location = new System.Drawing.Point(162, 59);
+            this.cmbCargo.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbCargo.Name = "cmbCargo";
+            this.cmbCargo.Size = new System.Drawing.Size(169, 28);
+            this.cmbCargo.TabIndex = 1;
+            // 
+            // cmbDepartamento
+            // 
+            this.cmbDepartamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDepartamento.FormattingEnabled = true;
+            this.cmbDepartamento.Location = new System.Drawing.Point(162, 6);
+            this.cmbDepartamento.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbDepartamento.Name = "cmbDepartamento";
+            this.cmbDepartamento.Size = new System.Drawing.Size(169, 28);
+            this.cmbDepartamento.TabIndex = 0;
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCargo.Location = new System.Drawing.Point(19, 62);
+            this.lblCargo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(57, 20);
+            this.lblCargo.TabIndex = 18;
+            this.lblCargo.Text = "Cargo";
+            // 
+            // lblDepartamento
+            // 
+            this.lblDepartamento.AutoSize = true;
+            this.lblDepartamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartamento.Location = new System.Drawing.Point(19, 6);
+            this.lblDepartamento.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDepartamento.Name = "lblDepartamento";
+            this.lblDepartamento.Size = new System.Drawing.Size(124, 20);
+            this.lblDepartamento.TabIndex = 19;
+            this.lblDepartamento.Text = "Departamento";
+            // 
+            // dgvSalarios
+            // 
+            this.dgvSalarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSalarios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSalarios.Location = new System.Drawing.Point(0, 135);
+            this.dgvSalarios.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvSalarios.Name = "dgvSalarios";
+            this.dgvSalarios.RowTemplate.Height = 28;
+            this.dgvSalarios.Size = new System.Drawing.Size(371, 349);
+            this.dgvSalarios.TabIndex = 1;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(4, 75);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(55, 20);
+            this.lblNome.TabIndex = 35;
+            this.lblNome.Text = "Nome";
+            // 
+            // pnlSalarios
+            // 
+            this.pnlSalarios.Controls.Add(this.dgvSalarios);
+            this.pnlSalarios.Controls.Add(this.pnlDepartamentoCargo);
+            this.pnlSalarios.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlSalarios.Location = new System.Drawing.Point(415, 0);
+            this.pnlSalarios.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlSalarios.Name = "pnlSalarios";
+            this.pnlSalarios.Size = new System.Drawing.Size(371, 484);
+            this.pnlSalarios.TabIndex = 26;
+            // 
+            // pnlDepartamentoCargo
+            // 
+            this.pnlDepartamentoCargo.Controls.Add(this.cmbCargo);
+            this.pnlDepartamentoCargo.Controls.Add(this.cmbDepartamento);
+            this.pnlDepartamentoCargo.Controls.Add(this.lblCargo);
+            this.pnlDepartamentoCargo.Controls.Add(this.lblDepartamento);
+            this.pnlDepartamentoCargo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlDepartamentoCargo.Location = new System.Drawing.Point(0, 0);
+            this.pnlDepartamentoCargo.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlDepartamentoCargo.Name = "pnlDepartamentoCargo";
+            this.pnlDepartamentoCargo.Size = new System.Drawing.Size(371, 135);
+            this.pnlDepartamentoCargo.TabIndex = 0;
+            // 
+            // FrmSalarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 484);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.cmbMes);
+            this.Controls.Add(this.lblMes);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.lblAno);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtNumeroUsuario);
+            this.Controls.Add(this.lblSobrenome);
+            this.Controls.Add(this.lblNumeroUsuario);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.pnlSalarios);
+            this.MaximizeBox = false;
+            this.Name = "FrmSalarios";
+            this.ShowIcon = false;
+            this.Text = "Salários";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalarios)).EndInit();
+            this.pnlSalarios.ResumeLayout(false);
+            this.pnlDepartamentoCargo.ResumeLayout(false);
+            this.pnlDepartamentoCargo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.ComboBox cmbMes;
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Label lblAno;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtNumeroUsuario;
+        private System.Windows.Forms.Label lblSobrenome;
+        private System.Windows.Forms.Label lblNumeroUsuario;
+        private System.Windows.Forms.ComboBox cmbCargo;
+        private System.Windows.Forms.ComboBox cmbDepartamento;
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.Label lblDepartamento;
+        private System.Windows.Forms.DataGridView dgvSalarios;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Panel pnlSalarios;
+        private System.Windows.Forms.Panel pnlDepartamentoCargo;
+    }
+}
---
+++ GerenciadorEmpresarial/Salarios/FrmSalarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorEmpresarial.Salarios
+{
+    public partial class FrmSalarios : Form
+    {
+        public FrmSalarios()
+        {
+            InitializeComponent();
+        }
+    }
+}
